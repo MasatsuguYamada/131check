ac29715fd3dfae8460f93adab8f865dcc37f2ae2
Masatsugu Yamada <masatsugu_yamada@mail.toyota.co.jp>
2024-09-10 11:12:32 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,71 +1,78 @@
-const kc_Enter = 13;
-window.onload = function () {
-  console.log("a");
-  setAttr_keydown(document.getElementById('topDv'));
+const input = [];
+
+for(let i=1; i<9; i++){
+  input[i] = document.getElementById("textBox"+i);
 }
 
-function setAttr_keydown(el) {
-  console.log(el);
-  if (el != null && el.tagName != 'BUTTON') {
-    console.log("a");
-    setEvent(el);
-    let cnt = el.children.length;
-    for (let i = 0; i < cnt; i++) {
-      setAttr_keydown(el.children[i]);
+const startBtnClick = () => {
+  check1 = false;
+  check2 = false;
+  input[1].focus();
+  for(let i = 1; i < 9; i++){
+    input[i].value = "";
+    input[i].style.backgroundColor = "";
+  }
+
+  operation1.style.backgroundColor = "";
+  operation1.style.border = "";
+  operation1.textContent = "１工程";
+
+  operation2.style.backgroundColor = "";
+  operation2.style.border = "";
+  operation2.textContent = "２工程";
+
+  audioStartBtn.currentTime = 0;
+  audioStartBtn.play();
+};
+
+const startBtn = document.getElementById("startBtn");
+startBtn.addEventListener("click", startBtnClick);
+
+const operation1 = document.getElementById("operation1");
+const operation2 = document.getElementById("operation2");
+
+const audioStartBtn = document.getElementById("startBtn_audio");
+const audioOp1 = document.getElementById("op1_audio");
+const audioOp2 = document.getElementById("op2_audio");
+const audioSuccess = document.getElementById("success_audio");
+const audioError = document.getElementById("error_audio");
+
+let check1 = false, check2 = false;
+
+function nextfeild(str) {
+
+  
+
+  if (str.value.length >= str.maxLength) {
+    for (let i = 1; i < input.length; i++) {
+      if (input[i] === str) {
+        if(input[i].value >= 20){
+          input[i].style.backgroundColor = "#75F94D";
+          audioSuccess.currentTime = 0;
+          audioSuccess.play();
+        } else {
+          input[i].style.backgroundColor = "#FF0000";
+          audioError.currentTime = 0;
+          audioError.play();
+        }
+        (input[i+1] || startBtn).focus();
+        break;
+      }
+    }
+    if(check1 === false && input[1].value >= 20 && input[2].value >= 20 && input[3].value >= 20 && input[4].value >= 20){
+      check1 = true;
+      operation1.textContent = "１工程完了";
+      operation1.style.backgroundColor = "#75F94D"
+      operation1.style.border = "solid"
+      audioOp1.play();
+    
+    }
+    if(check2 === false && input[5].value >= 20 && input[6].value >= 20 && input[7].value >= 20 && input[8].value >= 20){
+      check2 = true;
+      operation2.textContent = "２工程完了";
+      operation2.style.backgroundColor = "#75F94D"
+      operation2.style.border = "solid"
+      audioOp2.play();
     }
   }
-}
-
-
-function setEvent(el) {
-  console.log(el)
-  if (el == null) { return; }
-  el.addEventListener('keydown', moveNextElm);
-  el.addEventListener('focus', focusInput, false);
-}
-
-
-function moveNextElm(ev) {
-　if (ev.shiftKey == true) { return; }
-　if (ev.keyCode == kc_Enter) {
-　　try {
-　　　let el = getNextElm(ev.target);
-　　　if (el != null) { el.focus(); }
-　　　el = null;
-　　} catch (e) { }
-　}
-}
-
-
-function getNextElm(field) {
-　let form = field.form;
-　for (var e = 0; e < form.elements.length; e++) {
-　　if (field == form.elements[e]) { break; }
-　}
-　for (let n = e + 1; n < form.elements.length; n++) {
-　　if (form.elements[n % form.elements.length].style.display != 'none'
-　　&& form.elements[n % form.elements.length].type != 'hidden') {
-　　　return form.elements[n % form.elements.length];
-　　}
-　}
-　for (let n = 0; n < e; n++) {
-　　if (form.elements[n % form.elements.length].style.display != 'none'
-　　&& form.elements[n % form.elements.length].type != 'hidden') {
-　　　return form.elements[n % form.elements.length];
-　　}
-　}
-}
-
-
-function focusInput(ev) {
-　try { ev.target.setSelectionRange(0, ev.target.value.length); }
-　catch { }
-}
-
-
-document.onkeypress = enter;
-function enter() {
-　if (window.event.keyCode == kc_Enter) {
-　　return false;
-　}
 }
